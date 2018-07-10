8f7ec41b1aee7113e12a7d5eb40c0049a500976d
zhangyuanyuan@mengbitv.com <zhangyuanyuan>
2018-07-10 15:29:19 +0800

    充值规则，支付宝下单

--- app/src/main/java/com/whzl/mengbi/util/network/URLContentUtils.java
+++ app/src/main/java/com/whzl/mengbi/util/network/URLContentUtils.java
@@ -87,22 +87,6 @@ public class URLContentUtils{
      */
     public static final String OPEN_LOGIN="v1/user/open-login";
 
-
-    /**
-     * 获取房间信息
-     */
-    public static final String ROOM_INFO="v1/room/get-room-info";
-
-    /**
-     * 获取房间观看人数
-     */
-    public static final String AUDIENCE_COUNT="v1/room/total-audience";
-
-    /**
-     * 关注主播
-     */
-    public static final String FELLOW_HOST="v1/room/add-sub";
-
     /**
      * 移动端充值规则接口
      */
