768754480b7539b24e93362949a4f0dcce4bf53a
zhangyuanyuan@mengbitv.com <zhangyuanyuan>
2018-07-10 16:24:31 +0800

    个人信息 充值中心

---
+++ app/src/main/java/com/whzl/mengbi/ui/activity/UserInfoNickNameActivity.java
@@ -0,0 +1,15 @@
+package com.whzl.mengbi.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.whzl.mengbi.R;
+
+public class UserInfoNickNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info_nick_name_layout);
+    }
+}
