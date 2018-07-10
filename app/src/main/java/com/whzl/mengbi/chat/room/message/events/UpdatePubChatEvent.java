ab3998f7b1d45ed9f7db070f9232abc5a058ad73
wuyaobo <wuyaobo2008@gmail.com>
2018-07-10 10:27:08 +0800

    修改events

--- app/src/main/java/com/whzl/mengbi/chat/room/message/events/UpdatePubChatEvent.java
+++ app/src/main/java/com/whzl/mengbi/chat/room/message/events/UpdatePubChatEvent.java
@@ -2,7 +2,7 @@ package com.whzl.mengbi.chat.room.message.events;
 
 import com.whzl.mengbi.chat.room.message.messages.FillHolderMessage;
 
-
+//更新聊天消息的event
 public class UpdatePubChatEvent {
     private FillHolderMessage message;
 
