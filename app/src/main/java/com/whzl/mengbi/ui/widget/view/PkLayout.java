5433adba962428765fe790d64512208c51ba62f9
ChenLiang <chenliang@mengbitv.com>
2018-11-09 16:50:08 +0800

    PK进度条

--- app/src/main/java/com/whzl/mengbi/ui/widget/view/PkLayout.java
+++ app/src/main/java/com/whzl/mengbi/ui/widget/view/PkLayout.java
@@ -5,7 +5,10 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
+
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +18,21 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.whzl.mengbi.R;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+import com.whzl.mengbi.ui.adapter.base.BaseListAdapter;
+import com.whzl.mengbi.ui.adapter.base.BaseViewHolder;
+import com.whzl.mengbi.ui.widget.recyclerview.SpacesItemDecoration;
 import com.whzl.mengbi.util.LogUtils;
 import com.whzl.mengbi.util.glide.GlideImageLoader;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -28,8 +43,13 @@ import io.reactivex.disposables.Disposable;
  * @author nobody
  * @date 2018/9/24
  */
-public class PkLayout extends LinearLayout {
+public class PkLayout extends LinearLayout implements View.OnClickListener {
 
+    private final int PK_FIRST = 0;
+    private final int PK_SECOND = 1;
+    private final int PK_THIRD = 2;
+
+    private View inflate;
     private ProgressBar progressBar;
     private ImageView ivFount;
     private Context context;
@@ -52,6 +72,11 @@ public class PkLayout extends LinearLayout {
     private ValueAnimator animator;
     private TimeDwonListener listener;
 
+    private RelativeLayout rlPkProgress;
+    private PopupWindow popupWindow;
+    private RecyclerView myFollow;
+    private RecyclerView oppositeSide;
+    private List<String> list;
 
     public PkLayout(Context context) {
         this(context, null);
@@ -67,11 +92,13 @@ public class PkLayout extends LinearLayout {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PkLayout);
         initializeProgress = typedArray.getInt(R.styleable.PkLayout_progress, 50);
         init(context);
+        initPop(context);
     }
 
     private void init(Context context) {
         LayoutInflater from = LayoutInflater.from(context);
-        View inflate = from.inflate(R.layout.layout_pk_new, this, false);
+        inflate = from.inflate(R.layout.layout_pk_new, this, false);
+        inflate = from.inflate(R.layout.layout_pk_new, this, false);
         addView(inflate);
         progressBar = inflate.findViewById(R.id.pb_pk);
         ivFount = inflate.findViewById(R.id.iv_left);
@@ -89,6 +116,98 @@ public class PkLayout extends LinearLayout {
         tvRightScore = inflate.findViewById(R.id.tv_right_score);
         ivState = inflate.findViewById(R.id.iv_state);
         setProgress(initializeProgress);
+    }
+
+    private void initPop(Context context) {
+        // PopupWindow 显示PK排名
+        View popView = LayoutInflater.from(context).inflate(R.layout.pop_window_pk_rank, null);
+        popupWindow = new PopupWindow(popView, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true);
+        rlPkProgress = inflate.findViewById(R.id.rl_pk_progress);
+        rlPkProgress.setOnClickListener(this::onClick);
+        myFollow = popView.findViewById(R.id.rv_my_follow);
+        oppositeSide = popView.findViewById(R.id.rv_opposite_side);
+
+        list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i + "");
+        }
+
+        LinearLayoutManager followManager = new LinearLayoutManager(context);
+        followManager.setOrientation(HORIZONTAL);
+        followManager.setReverseLayout(true);
+        myFollow.setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        myFollow.setLayoutManager(followManager);
+        LinearLayoutManager oppositeManager = new LinearLayoutManager(context);
+        oppositeManager.setOrientation(HORIZONTAL);
+        oppositeSide.setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        oppositeSide.setLayoutManager(oppositeManager);
+
+        BaseListAdapter myFollowAdapter = new BaseListAdapter() {
+            @Override
+            protected int getDataCount() {
+                return list == null ? 0 : 5;
+            }
+
+            @Override
+            protected BaseViewHolder onCreateNormalViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_pk_follow, parent, false);
+                return new PKViewHolder(itemView);
+            }
+        };
+        myFollow.setAdapter(myFollowAdapter);
+
+        BaseListAdapter oppositeAdapter = new BaseListAdapter() {
+            @Override
+            protected int getDataCount() {
+                return list == null ? 0 : 5;
+            }
+
+            @Override
+            protected BaseViewHolder onCreateNormalViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_pk_opposite, parent, false);
+                return new PKOppoViewHolder(itemView);
+            }
+        };
+
+        oppositeSide.setAdapter(oppositeAdapter);
+    }
+
+    class PKViewHolder extends BaseViewHolder {
+        ImageView ivPkHead;
+        ImageView ivPkLevel;
+        TextView tvPkCount;
+
+        public PKViewHolder(View itemView) {
+            super(itemView);
+            ivPkHead = itemView.findViewById(R.id.iv_circle_head);
+            ivPkLevel = itemView.findViewById(R.id.iv_pk_level);
+            tvPkCount = itemView.findViewById(R.id.tv_pk_count);
+        }
+
+        @Override
+        public void onBindViewHolder(int position) {
+            tvPkCount.setText(list.get(position));
+            showRanking(position, ivPkLevel);
+        }
+    }
+
+    class PKOppoViewHolder extends BaseViewHolder {
+        ImageView ivPkHead;
+        ImageView ivPkLevel;
+        TextView tvPkCount;
+
+        public PKOppoViewHolder(View itemView) {
+            super(itemView);
+            ivPkHead = itemView.findViewById(R.id.iv_circle_head);
+            ivPkLevel = itemView.findViewById(R.id.iv_pk_level);
+            tvPkCount = itemView.findViewById(R.id.tv_pk_count);
+        }
+
+        @Override
+        public void onBindViewHolder(int position) {
+            tvPkCount.setText(list.get(position));
+            showRanking(position, ivPkLevel);
+        }
     }
 
     public void setStateImg(Object object) {
@@ -133,15 +252,17 @@ public class PkLayout extends LinearLayout {
                 .subscribe(aLong -> {
                     LogUtils.e("ssssss  state" + aLong);
 //                    tvTime.setText(context.getString(R.string.pk_time, (9 - aLong) / 60, (9 - aLong) % 60));
-                    if (aLong<second-1) {
-                        tvTime.setText(state + context.getString(R.string.pk_time, (second - aLong - 1) / 60, (second - aLong - 1) % 60));
-                    }
-                    if (aLong == second - 11 && "PK进行中 ".equals(state) && listener != null) {
-                        listener.onTimeDownListener();
-                    }
-                    if (aLong >= second - 1) {
-                        LogUtils.e("ssssss  state dispose");
-                        disposable.dispose();
+
+                    if (aLong < second - 1) {
+                        if (aLong < second - 1) {
+                            tvTime.setText(state + context.getString(R.string.pk_time, (second - aLong - 1) / 60, (second - aLong - 1) % 60));
+                        }
+                        if (aLong == second - 11 && "PK进行中 ".equals(state) && listener != null) {
+                            listener.onTimeDownListener();
+                        }
+                        if (aLong >= second - 1) {
+                            LogUtils.e("ssssss  state dispose");
+                            disposable.dispose();
 //                        ivLeftLead.setVisibility(GONE);
 //                        ivRightLead.setVisibility(GONE);
 //                        if (progressBar.getProgress() == 50) {
@@ -154,9 +275,10 @@ public class PkLayout extends LinearLayout {
 //                        if (progressBar.getProgress() > 50) {
 //                            setLeftWin();
 //                            ivLeftCrown.setVisibility(VISIBLE);
-//                        }
+                        }
                     }
                 });
+
     }
 
     public void setAnimation(final int mProgressBar) {
@@ -220,6 +342,10 @@ public class PkLayout extends LinearLayout {
         if (disposable2 != null) {
             disposable2.dispose();
         }
+
+        if (popupWindow != null) {
+            popupWindow.dismiss();
+        }
     }
 
     public void setLeftLead() {
@@ -264,8 +390,24 @@ public class PkLayout extends LinearLayout {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_pk_progress:
+                if (popupWindow != null) {
+                    if (popupWindow.isShowing()) {
+                        popupWindow.dismiss();
+                    } else {
+                        showPopupWindow(v);
+                    }
+                }
+                break;
+        }
+    }
+
     public interface TimeDwonListener {
         void onTimeDownListener();
+
     }
 
     public void reset() {
@@ -292,4 +434,32 @@ public class PkLayout extends LinearLayout {
     public void setRightClickListener(OnClickListener listener) {
         ivRight.setOnClickListener(listener);
     }
+
+
+    private void showPopupWindow(View view) {
+        popupWindow.setOutsideTouchable(false);
+        popupWindow.setFocusable(false);
+        popupWindow.setTouchInterceptor(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+        popupWindow.showAsDropDown(view);
+    }
+
+    private void showRanking(int ranking, ImageView imageView) {
+        switch (ranking) {
+            case PK_FIRST:
+                GlideImageLoader.getInstace().displayImage(context, R.drawable.ic_pk_1, imageView);
+                break;
+            case PK_SECOND:
+                GlideImageLoader.getInstace().displayImage(context, R.drawable.ic_pk_2, imageView);
+                break;
+            case PK_THIRD:
+                GlideImageLoader.getInstace().displayImage(context, R.drawable.ic_pk_3, imageView);
+                break;
+        }
+    }
 }
+
