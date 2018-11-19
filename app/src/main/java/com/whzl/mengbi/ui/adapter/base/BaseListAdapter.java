5655018327a4478c1a9632a0cadb34143bb166d5
nobody <1132166858@qq.com>
2018-11-19 11:05:47 +0800

    follow

--- app/src/main/java/com/whzl/mengbi/ui/adapter/base/BaseListAdapter.java
+++ app/src/main/java/com/whzl/mengbi/ui/adapter/base/BaseListAdapter.java
@@ -1,15 +1,12 @@
 package com.whzl.mengbi.ui.adapter.base;
 
-import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.whzl.mengbi.R;
 
 
