d53a707df407066e15cec53a7bcd584e62c252c9
nobody <1132166858@qq.com>
2018-12-06 14:02:08 +0800

    week star in play

--- app/src/main/java/com/whzl/mengbi/chat/room/message/messagesActions/WeekStarAction.java
+++ app/src/main/java/com/whzl/mengbi/chat/room/message/messagesActions/WeekStarAction.java
@@ -2,7 +2,12 @@ package com.whzl.mengbi.chat.room.message.messagesActions;
 
 import android.content.Context;
 
+import com.whzl.mengbi.chat.room.message.events.WeekStarEvent;
+import com.whzl.mengbi.chat.room.message.messageJson.WeekStarJson;
+import com.whzl.mengbi.util.GsonUtils;
 import com.whzl.mengbi.util.LogUtils;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * @author nobody
@@ -11,6 +16,11 @@ import com.whzl.mengbi.util.LogUtils;
 public class WeekStarAction implements Actions {
     @Override
     public void performAction(String msgStr, Context context) {
-        LogUtils.e("sssssssssss  "+msgStr);
+        LogUtils.e("sssssssss   "+msgStr);
+        WeekStarJson weekStarJson = GsonUtils.GsonToBean(msgStr, WeekStarJson.class);
+        if (null == weekStarJson || weekStarJson.context == null) {
+            return;
+        }
+        EventBus.getDefault().post(new WeekStarEvent(context,weekStarJson));
     }
 }
---
+++ app/src/main/java/com/whzl/mengbi/gift/WeekStarControl.java
@@ -0,0 +1,102 @@
+package com.whzl.mengbi.gift;
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.whzl.mengbi.R;
+import com.whzl.mengbi.chat.room.message.events.WeekStarEvent;
+import com.whzl.mengbi.chat.room.message.messageJson.WeekStarJson;
+import com.whzl.mengbi.chat.room.util.LevelUtil;
+import com.whzl.mengbi.ui.widget.view.WeekStarView;
+import com.whzl.mengbi.util.glide.GlideImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author nobody
+ * @date 2018/11/5
+ */
+public class WeekStarControl {
+    RelativeLayout flEnter;
+    WeekStarView tvEnter;
+    ImageView ivEnter;
+    Context context;
+
+    public WeekStarControl(Context context) {
+        this.context = context;
+    }
+
+
+    public void setIvEnter(ImageView ivEnter) {
+        this.ivEnter = ivEnter;
+    }
+
+
+    public void setRlEnter(RelativeLayout llEnter) {
+        this.flEnter = llEnter;
+    }
+
+    public void setTvEnter(WeekStarView tvEnter) {
+        this.tvEnter = tvEnter;
+    }
+
+    private List<WeekStarEvent> list = new ArrayList<>();
+
+    private boolean isPlay = false;
+
+    public void showEnter(WeekStarEvent welcomeMsg) {
+        list.add(welcomeMsg);
+        if (!isPlay && list.size() != 0) {
+            startAnimal();
+        }
+
+    }
+
+    private void startAnimal() {
+        isPlay = true;
+        flEnter.setVisibility(View.VISIBLE);
+        initTv(list.get(0));
+        tvEnter.init();
+        GlideImageLoader.getInstace().loadOneTimeGif(context, R.drawable.bg_zhouxing, ivEnter, new GlideImageLoader.GifListener2() {
+            @Override
+            public void gifPlayComplete() {
+                tvEnter.startScroll();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        outAnim();
+                    }
+                }, 5000);
+            }
+        });
+    }
+
+    private void initTv(WeekStarEvent weekStarEvent) {
+        if (weekStarEvent.weekStarJson != null) {
+            WeekStarJson.ContextBean bean = weekStarEvent.weekStarJson.context;
+            tvEnter.setText(LevelUtil.getImageResourceSpanByHeight(this.context, R.drawable.ic_week_star_play, 12));
+            tvEnter.append(" 恭喜 ");
+            tvEnter.append(bean.nickName);
+            tvEnter.append(" 在周星礼物 ");
+            tvEnter.append("xing");
+            tvEnter.append(" 争夺中，上升到第一名");
+        }
+    }
+
+    private void outAnim() {
+        list.remove(0);
+        tvEnter.stopScroll();
+        tvEnter.setText("");
+        if (list.size() > 0) {
+            startAnimal();
+        } else {
+            isPlay = false;
+            flEnter.setVisibility(View.GONE);
+        }
+    }
+
+}
