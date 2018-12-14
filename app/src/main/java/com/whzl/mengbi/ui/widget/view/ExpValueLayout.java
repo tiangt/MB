d92f530dc67994fddcdbfc51d83898616371fec9
ChenLiang <chenliang@mengbitv.com>
2018-12-14 15:36:59 +0800

    level icon

--- app/src/main/java/com/whzl/mengbi/ui/widget/view/ExpValueLayout.java
+++ app/src/main/java/com/whzl/mengbi/ui/widget/view/ExpValueLayout.java
@@ -87,17 +87,29 @@ public class ExpValueLayout extends LinearLayout {
         getLevel();
         if ("ANCHOR_LEVEL".equals(levelType)) {
             ivLevelNow.setImageResource(ResourceMap.getResourceMap().getAnchorLevelIcon(levelValue));
-            ivLevelNext.setImageResource(ResourceMap.getResourceMap().getAnchorLevelIcon(levelValue + 1));
+            if (levelValue == 50) {
+                ivLevelNext.setVisibility(INVISIBLE);
+            } else {
+                ivLevelNext.setImageResource(ResourceMap.getResourceMap().getAnchorLevelIcon(levelValue + 1));
+            }
         } else if ("ROYAL_LEVEL".equals(levelType)) {
             if (levelValue == 0) {
                 ivLevelNow.setVisibility(INVISIBLE);
             } else {
                 ivLevelNow.setImageResource(ResourceMap.getResourceMap().getRoyalLevelIcon(levelValue));
             }
-            ivLevelNext.setImageResource(ResourceMap.getResourceMap().getRoyalLevelIcon(levelValue + 1));
+            if (levelValue == 7) {
+                ivLevelNext.setVisibility(INVISIBLE);
+            } else {
+                ivLevelNext.setImageResource(ResourceMap.getResourceMap().getRoyalLevelIcon(levelValue + 1));
+            }
         } else if ("USER_LEVEL".equals(levelType)) {
             ivLevelNow.setImageResource(ResourceMap.getResourceMap().getUserLevelIcon(levelValue));
-            ivLevelNext.setImageResource(ResourceMap.getResourceMap().getUserLevelIcon(levelValue + 1));
+            if (levelValue == 37) {
+                ivLevelNext.setVisibility(INVISIBLE);
+            } else {
+                ivLevelNext.setImageResource(ResourceMap.getResourceMap().getUserLevelIcon(levelValue + 1));
+            }
         }
     }
 
@@ -107,27 +119,45 @@ public class ExpValueLayout extends LinearLayout {
             if ("GIFT_EXP".equals(expType)) {
                 sjExpvalue = listBeans.get(j).getSjExpvalue();
                 sjNeedExpValue = listBeans.get(j).getSjNeedExpValue();
-                tpAnchorLevel.setMax((int) (sjNeedExpValue));
-                if (sjExpvalue < sjNeedExpValue) {
-                    tvPb.setText(context.getString(R.string.anchor_grade, (sjNeedExpValue - sjExpvalue)));
-                    tpAnchorLevel.setProgress((int) sjExpvalue);
+                if (levelValue == 50) {
+                    tpAnchorLevel.setMax(100);
+                    tpAnchorLevel.setProgress(100);
+                    tvPb.setText("您已达到最高主播等级");
+                } else {
+                    tpAnchorLevel.setMax((int) (sjNeedExpValue));
+                    if (sjExpvalue < sjNeedExpValue) {
+                        tvPb.setText(context.getString(R.string.anchor_grade, (sjNeedExpValue - sjExpvalue)));
+                        tpAnchorLevel.setProgress((int) sjExpvalue);
+                    }
                 }
             } else if ("ROYAL_EXP".equals(expType)) {
                 sjExpvalue1 = listBeans.get(j).getSjExpvalue();
                 sjNeedExpValue1 = listBeans.get(j).getSjNeedExpValue();
-                tpRoyalLevel.setMax((int) (sjNeedExpValue1));
-                if (sjExpvalue1 < sjNeedExpValue1) {
-                    tvPb.setText(context.getString(R.string.royal_grade, (sjNeedExpValue1 - sjExpvalue1)));
-                    tpRoyalLevel.setProgress((int) sjExpvalue1);
+                if (levelValue == 7) {
+                    tpAnchorLevel.setMax(100);
+                    tpAnchorLevel.setProgress(100);
+                    tvPb.setText("您已达到最高贵族等级");
+                } else {
+                    tpRoyalLevel.setMax((int) (sjNeedExpValue1));
+                    if (sjExpvalue1 < sjNeedExpValue1) {
+                        tvPb.setText(context.getString(R.string.royal_grade, (sjNeedExpValue1 - sjExpvalue1)));
+                        tpRoyalLevel.setProgress((int) sjExpvalue1);
+                    }
                 }
                 tpRoyalLevel.setVisibility(VISIBLE);
             } else if ("USER_EXP".equals(expType)) {
                 sjExpvalue2 = listBeans.get(j).getSjExpvalue();
                 sjNeedExpValue2 = listBeans.get(j).getSjNeedExpValue();
-                tpUserLevel.setMax((int) (sjNeedExpValue2));
-                if (sjExpvalue2 < sjNeedExpValue2) {
-                    tvPb.setText(context.getString(R.string.user_grade, (sjNeedExpValue2 - sjExpvalue2)));
-                    tpUserLevel.setProgress((int) sjExpvalue2);
+                if (levelValue == 37) {
+                    tpAnchorLevel.setMax(100);
+                    tpAnchorLevel.setProgress(100);
+                    tvPb.setText("您已达到最高用户等级");
+                } else {
+                    tpUserLevel.setMax((int) (sjNeedExpValue2));
+                    if (sjExpvalue2 < sjNeedExpValue2) {
+                        tvPb.setText(context.getString(R.string.user_grade, (sjNeedExpValue2 - sjExpvalue2)));
+                        tpUserLevel.setProgress((int) sjExpvalue2);
+                    }
                 }
                 tpUserLevel.setVisibility(VISIBLE);
             }
