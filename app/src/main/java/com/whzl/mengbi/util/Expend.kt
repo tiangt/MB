b0584da8d9e192ed3958f0cbb66b6eb392fec1b3
nobody <1132166858@qq.com>
2019-03-15 11:19:01 +0800

    add

---
+++ app/src/main/java/com/whzl/mengbi/util/Expend.kt
@@ -0,0 +1,33 @@
+package com.whzl.mengbi.util
+
+import android.view.View
+import com.whzl.mengbi.util.Expend.SPACE_TIME
+import com.whzl.mengbi.util.Expend.hash
+import com.whzl.mengbi.util.Expend.lastClickTime
+
+/**
+ *
+ * @author nobody
+ * @date 2019/3/14
+ */
+object Expend {
+    var hash: Int = 0
+    var lastClickTime: Long = 0
+    var SPACE_TIME: Long = 300
+}
+
+infix fun View.clickDelay(clickAction: () -> Unit) {
+    this.setOnClickListener {
+        if (this.hashCode() != hash) {
+            hash = this.hashCode()
+            lastClickTime = System.currentTimeMillis()
+            clickAction()
+        } else {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastClickTime > SPACE_TIME) {
+                lastClickTime = System.currentTimeMillis()
+                clickAction()
+            }
+        }
+    }
+}
