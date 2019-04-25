f3704e1a4ef79a734528b881280bdc1aef068fde
nobody <1132166858@qq.com>
2019-04-25 11:02:01 +0800

    feed back

--- app/src/main/java/com/whzl/mengbi/ui/activity/FeedbackActivity.java
+++ app/src/main/java/com/whzl/mengbi/ui/activity/FeedbackActivity.java
@@ -2,19 +2,27 @@ package com.whzl.mengbi.ui.activity;
 
 import android.graphics.Color;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.google.gson.JsonElement;
 import com.whzl.mengbi.R;
 import com.whzl.mengbi.api.Api;
 import com.whzl.mengbi.config.SpConfig;
 import com.whzl.mengbi.ui.activity.base.BaseActivity;
+import com.whzl.mengbi.util.DateUtils;
 import com.whzl.mengbi.util.KeyBoardUtil;
 import com.whzl.mengbi.util.SPUtils;
 import com.whzl.mengbi.util.network.retrofit.ApiFactory;
 import com.whzl.mengbi.util.network.retrofit.ApiObserver;
 import com.whzl.mengbi.util.network.retrofit.ParamsUtils;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 import butterknife.BindView;
@@ -31,8 +39,10 @@ public class FeedbackActivity extends BaseActivity {
     EditText etFeedback;
     @BindView(R.id.et_contact)
     EditText etContact;
-    @BindView(R.id.et_email)
-    EditText etEmail;
+    @BindView(R.id.tv_time_feed_back)
+    TextView tvTimeFeedBack;
+
+    private TimePickerView pvTime;
 
     @Override
     protected void setupContentView() {
@@ -42,6 +52,7 @@ public class FeedbackActivity extends BaseActivity {
     @Override
     protected void setupView() {
         getTitleRightText().setTextColor(Color.parseColor("#ff2b3f"));
+        tvTimeFeedBack.setText(DateUtils.getStringDate( ));
     }
 
     @Override
@@ -49,19 +60,19 @@ public class FeedbackActivity extends BaseActivity {
 
     }
 
-    @OnClick(R.id.user_settings_feed_back_submit_but)
-    public void onClick() {
+    @Override
+    protected void onToolbarMenuClick() {
+        super.onToolbarMenuClick();
         KeyBoardUtil.hideInputMethod(this);
         String content = etFeedback.getText().toString().trim();
         String contact = etContact.getText().toString().trim();
-        String email = etEmail.getText().toString().trim();
         if (TextUtils.isEmpty(content)) {
             showToast("请输入反馈内容");
             return;
         }
 
         showLoading("请稍后...");
-        feedback(content, contact, email);
+//        feedback(content, contact, email);
     }
 
     private void feedback(String content, String qq, String email) {
@@ -89,5 +100,51 @@ public class FeedbackActivity extends BaseActivity {
                         dismissLoading();
                     }
                 });
+    }
+
+
+    @OnClick({R.id.rl_time_feed_back})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.rl_time_feed_back:
+                showDatePick();
+                break;
+        }
+    }
+
+    private void showDatePick() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {//选中事件回调
+                tvTimeFeedBack.setText(DateUtils.getTime2(date));
+            }
+        })
+
+                .setDate(calendar)
+                .setType(new boolean[]{true, true, true, true, true, true})// 默认全部显示
+                .setCancelText("取消")//取消按钮文字
+                .setSubCalSize(13)
+                .setSubmitText("确定")//确认按钮文字
+                .setTitleSize(15)//标题文字大小
+                .setTitleText("出生日期")//标题文字
+                .isCyclic(true)//是否循环滚动
+                .setLineSpacingMultiplier(2)
+                .setTitleBgColor(Color.WHITE)
+                .setTitleColor(Color.parseColor("#70000000"))//标题文字颜色
+                .setSubmitColor(Color.parseColor("#ff2b3f"))//确定按钮文字颜色
+                .setCancelColor(Color.parseColor("#70000000"))//取消按钮文字颜色
+                .setDividerColor(Color.parseColor("#cdcdcd"))
+                .setContentTextSize(15)
+                .setTextColorCenter(Color.BLACK)
+                .setBgColor(Color.WHITE)//滚轮背景颜色 Night mode
+                .setLabel("年", "月", "日", "时", "分", "秒")//默认设置为年月日时分秒
+                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                .isDialog(false)//是否显示为对话框样式
+                .build();
+
+        pvTime.show();
+
     }
 }
